38b3fe3204f5978420c563a968609e8ba229e440
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-02-11 17:14:26 +0530

    Permutations_with_unique_elements
    Added Solution class from Leetcode

---
+++ Recursion + Backtracking/Permutations_with_unique_elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void helper(int pos, vector<int> &nums, vector<vector<int>> &ans){
+        if(pos == nums.size()){
+            // come at the end of the branch.
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=pos; i<nums.size(); i++){
+            swap(nums[pos], nums[i]);        // keeping all elements in the pos.
+            helper(pos+1, nums, ans); 
+            swap(nums[i], nums[pos]);       // cleaning is done.
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;     // storing my answer here
+        helper(0, nums, ans);
+        return ans;
+    }
+};
