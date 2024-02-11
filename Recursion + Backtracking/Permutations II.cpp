74a496aeb1f9696576bf15b9ec38033fb9e8c7d6
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-02-11 17:15:51 +0530

    Create Permutations II.cpp
    Problem Link :
    https://leetcode.com/problems/permutations-ii/

---
+++ Recursion + Backtracking/Permutations II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(int pos, vector<int> &nums, vector<vector<int>> &ans){
+        if(pos == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        unordered_set<int> u_set;       // that's tracking all the elements that has already been considered.
+        for(int i=pos; i<nums.size(); i++){
+            if(u_set.find(nums[i]) == u_set.end())    // whether nums[i] has already been considered for that position or not.
+            {
+                u_set.insert(nums[i]);
+                swap(nums[pos], nums[i]);
+                helper(pos+1, nums, ans);
+                swap(nums[i], nums[pos]);
+            } 
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(0, nums, ans);
+        return ans;
+    }
+};
