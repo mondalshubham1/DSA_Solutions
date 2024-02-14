7afef6371a2c6a69e2bfbaf854ab0d2c9bdea2d3
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-02-15 00:23:05 +0530

    Update Coin Change.cpp

--- Dynammic Programming/Coin Change.cpp
+++ Dynammic Programming/Coin Change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> memset(amount+1, vector<int>(coins.size(), 0));
+        vector<vector<int>> memset(amount+1, vector<int>(coins.size(), 0));                 // memoization table
         int cols = coins.size();
         int rows = amount+1;
         for(int j=cols-1; j>=0 ; j--) {
             for(int i=1; i<rows ; i++) {
-                memset[i][j] = INT_MAX;
-                if(i >= coins[j] && memset[i-coins[j]][j] != -1) {
+                memset[i][j] = INT_MAX;                                                   // memset[i][j] = minimum number of coins that's needed to make amount i from all the coin[j:end]
+                if(i >= coins[j] && memset[i-coins[j]][j] != -1) {                        // pick the coin at jth index
                     int f = 1+memset[i-coins[j]][j];
                     memset[i][j] = f;
                 }
-                if(j+1 < cols && memset[i][j+1] != -1) {
+                if(j+1 < cols && memset[i][j+1] != -1) {                                 // don't pick
                     int s = memset[i][j+1];
                     memset[i][j] = min(memset[i][j], s);
                 }
