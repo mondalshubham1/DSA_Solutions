c4b93b7667e34176f2ac0542bf3b0069929c6efb
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-03-23 21:12:18 +0530

    Create Same Tree.cpp

---
+++ Trees/Beginner/Same Tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL){
+            if(p==NULL && q==NULL)     // both p & q are NULL
+                return true;
+            else                        // exactly one of them is NULL
+                return false;
+        }
+        return (p->val==q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);  
+    }
+};
