e32bd0d973fe98289f547d983d7947caa4ad61f0
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-03-26 07:23:51 +0530

    Create Subsets.cpp

---
+++ Recursion + Backtracking/Subsets.cpp
@@ -0,0 +1,24 @@
+// Problem Link : https://leetcode.com/problems/subsets/description/
+
+class Solution {
+public:
+    void helper(int pos, vector<int>& partial_ans, vector<int> nums, vector<vector<int>>& ans) {
+        if(pos == nums.size()) {
+            ans.push_back(partial_ans);
+            return;
+        }
+
+        helper(pos+1, partial_ans, nums, ans);          // don't take nums[pos]
+
+        partial_ans.push_back(nums[pos]);
+        helper(pos+1, partial_ans, nums, ans);          // take nums[pos]
+        partial_ans.pop_back();                         // Cleaning step in Backtracking
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> partial_ans;
+        helper(0, partial_ans, nums, ans);
+        return ans;
+    }
+};
