a2fbb923d8f2c49b1550b33e9cbe7059412ff85b
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-04-01 08:33:14 +0530

    Update Longest Palindrome.cpp

--- Greedy/Longest Palindrome.cpp
+++ Greedy/Longest Palindrome.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         map<char, int> mp;
-        for(auto x: s){
+        for(auto x: s){                                  // make a frequency map
             if(mp.find(x) == mp.end())
                 mp[x] = 1;
             else
@@ -12,7 +12,10 @@ public:
         }
         int even_length = 0;
         int oddLengthPresent = 0;
-        for(auto x: mp){
+        
+        // In a palindrome, there can be at max one character with odd frequency and rest of the character frequency has to be even
+        
+        for(auto x: mp){                                // for all odd frequencies, you can add the max even number, i.e. odd frequency-1.       
             if(x.second % 2) {
                 oddLengthPresent = 1;
                 even_length += x.second-1;
