f37d1a13c03e4b3389e991feb80651e07bafd268
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-04-04 18:54:58 +0530

    Create Maximum Sum With Exactly K Elements.cpp

---
+++ Greedy/Maximum Sum With Exactly K Elements.cpp
@@ -0,0 +1,14 @@
+// Problem Link : https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/description/
+
+class Solution {
+public:
+    int maximizeSum(vector<int>& nums, int k) {
+        int max_value = INT_MIN;
+        for(int i=0; i<nums.size(); i++) {
+            max_value = max(max_value, nums[i]);
+        }
+        int ans = (max_value+k-1)*(max_value+k) - (max_value)*(max_value-1);
+        ans /= 2;
+        return ans; 
+    }
+};
