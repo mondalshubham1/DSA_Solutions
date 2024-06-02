ebec8d76c05d4cde112515a8986a5cfb63c7effd
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-06-02 09:50:31 +0530

    Create XOR Operation in an Array.cpp
    Added code

---
+++ Bit Manipulation/XOR Operation in an Array.cpp
@@ -0,0 +1,11 @@
+// Problem Link : https://leetcode.com/problems/xor-operation-in-an-array/description/
+
+class Solution {
+    public int xorOperation(int n, int start) {
+        int ans = 0;
+        for(int i=0; i<n ; i++){
+            ans ^= start + 2*i;
+        }
+        return ans;
+    }
+}
