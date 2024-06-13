fea1fdd2bcbaa734b789d1cab8c4211424466f88
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-06-13 11:04:48 +0530

    Create Coin Change.cpp
    Added code

---
+++ Dynammic Programming/Beginner/Coin Change.cpp
@@ -0,0 +1,28 @@
+// Problem Link : https://leetcode.com/problems/coin-change/description/
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> memo(amount+1, INT_MAX);
+      
+        /*
+          memo[i] = minimum coins needed for making i amount.
+          You choose each coin, say coins[j] and find the minimum coins needed to make the i-coins[j], then find the minimum among all of them 
+          memo[i] = min(memo[i-coins[j]])  for all 0<=j<n
+        */
+      
+        memo[0] = 0;
+        for(int i=1; i<=amount; i++) {
+            for(int j=0; j<coins.size(); j++) {
+                if(coins[j] <= i && memo[i-coins[j]] != INT_MAX)
+                    memo[i] = min(memo[i], memo[i-coins[j]]+1);
+            }
+        }
+
+        for(int x: memo)
+            cout << x << endl;
+
+        return memo[amount]==INT_MAX ? -1 : memo[amount];
+        
+    }
+};
