e0753e39abaa67f46c8626bac4ce307bc8825707
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-07-07 00:08:15 +0530

    Update Container With Most Water.cpp
    Added code logic

--- Two Pointer/Container With Most Water.cpp
+++ Two Pointer/Container With Most Water.cpp
@@ -6,9 +6,14 @@ public:
         int ans = INT_MIN;
 
         while(i < j) {
-            ans = max(ans, min(height[j], height[i])*(j-i));
-
-            if(height[i] <= height[j]) {
+            ans = max(ans, min(height[j], height[i])*(j-i));          // Find the water you can store between ith line and jth line
+            
+            /*
+                If height of ith line is smaller then there cannot be any other line to the right of i with height smaller than ith line that will give me more water storage.
+                Hence I will increment my i to the point till I get height more than the original.
+            */
+            
+            if(height[i] <= height[j]) {                              
                 int m=i;
                 while(m<j && height[m]<=height[i]){
                     m++;
