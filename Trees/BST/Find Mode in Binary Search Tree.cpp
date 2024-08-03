f7607643e172c65508bcda92d545599f33b22e3d
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-08-03 09:31:12 +0530

    Create Find Mode in Binary Search Tree.cpp
    Added code

---
+++ Trees/BST/Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,57 @@
+// Problem Link : https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {                 // It's very similar to finding mode in a sorted array, inorder traversal of a BST will take me through the tree in sorted order.
+public:
+    void inorder(TreeNode* root, int& prev, int& prev_count, int& mode_count, vector<int>& ans) {
+        if(root == NULL) {
+            return;
+        }
+        inorder(root->left, prev, prev_count, mode_count, ans);
+        if(root->val != prev) {                                               
+            if(prev_count > mode_count) {
+                while(ans.size() > 0) {
+                    ans.pop_back();
+                }
+                ans.push_back(prev);
+                mode_count = prev_count;
+            }
+            else if(prev_count == mode_count && prev!=INT_MIN)
+                ans.push_back(prev);
+
+            prev_count = 1;
+        }
+        else {
+            prev_count++;
+        }
+        prev = root->val;
+        inorder(root->right, prev, prev_count, mode_count, ans);
+    }
+    vector<int> findMode(TreeNode* root) {
+        int prev_count=0;
+        int prev=INT_MIN;
+        int mode_count=0;
+        vector<int> ans;
+        inorder(root, prev, prev_count, mode_count, ans);
+        if(prev_count > mode_count) {
+            while(ans.size() > 0) {
+                ans.pop_back();
+            }
+            ans.push_back(prev);
+        }
+        else if(prev_count == mode_count && prev!=INT_MIN)
+            ans.push_back(prev);
+
+        return ans;
+    }
+};
