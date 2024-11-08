c973b90d9f777951c777a31a546ef4a8179f1beb
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-11-08 22:41:12 +0530

    Update Unique Paths III.cpp
    Added comments

--- Recursion + Backtracking/Unique Paths III.cpp
+++ Recursion + Backtracking/Unique Paths III.cpp
@@ -3,10 +3,12 @@
 class Solution {
 public:
     void helper(int i, int j, vector<vector<int>>& grid, int& count, int empty_sq, int &ans){
-        count++;          // i'll land in that i,j if that's a non-obstacle square.
+        // i, j is the starting point, grid is the input matrix, count stores the number of empty squares I've encountered in the path before reaching (i,j)
+        
+        count++;          // I'll land in that i,j if that's a non-obstacle square.
 
-        if((grid[i][j] == 2) && count == empty_sq){
-            ans++;
+        if((grid[i][j] == 2) && count == empty_sq){     // I've reached the end point using a valid path  
+            ans++;    
             count--;
             return;
         }
@@ -20,7 +22,6 @@ public:
         if(i+1<grid.size() && grid[i+1][j] != -1)
             helper(i+1, j, grid, count, empty_sq, ans);
         
-
         // up
         if(i-1>=0 && grid[i-1][j] != -1)
             helper(i-1, j, grid, count, empty_sq, ans);
@@ -33,11 +34,11 @@ public:
         if(j-1>=0 && grid[i][j-1] != -1)
             helper(i, j-1, grid, count, empty_sq, ans);
 
-
-
+        // No more decisions to explore so perform Backtracking cleaning step.
         count--;
-        grid[i][j] = og_grid_val;               // Backtracking cleaning step.
+        grid[i][j] = og_grid_val;       // restoring the original grid value         
     }
+
     int uniquePathsIII(vector<vector<int>>& grid) {
         int count = 0;        // maintained a variable count that will tell me how many non-obstacle points I've encountered in a certain path.
         int ans = 0;          // final answer that I'll return, it'll get incremented when all the conditions are met.
@@ -45,10 +46,10 @@ public:
         int empty_sq = 0;
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[0].size(); j++){
-                if(grid[i][j] >= 0)
+                if(grid[i][j] >= 0)   // 0, 1, 2 all are non-onstacle.
                     empty_sq++;
             }
-        }        // count number of non-obstacle points.
+        }        // count number of non-obstacle points, how many 0's are there
     
 
         int start_i, start_j;
@@ -62,6 +63,6 @@ public:
         }           // find the start position
 
         helper(start_i, start_j, grid, count, empty_sq, ans);
-        return ans;
+        return ans;    // tells me how many valid paths I've found
     }
 };
