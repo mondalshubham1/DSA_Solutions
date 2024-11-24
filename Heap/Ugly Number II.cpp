eaf3d72ac8190ddcd74ec625388e54e5c9561cf1
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-11-24 12:17:02 +0530

    Update Ugly Number II.cpp
    Added code

--- Heap/Ugly Number II.cpp
+++ Heap/Ugly Number II.cpp
@@ -4,14 +4,13 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         int count = 0;
-        priority_queue<long long> pq;
+        priority_queue<long long> pq;      // maintain a heap of elements containing the next possible Ugly Number
         pq.push(-1);
-        unordered_set<long long> uset;
+        unordered_set<long long> uset;     // to have a track which elements are already there in your heap
         uset.insert(1);
 
-        while(count < n-1) {
+        while(count < n-1) {               // Take out the first n-1 Ugly numbers
             long long x = pq.top();
-            //cout << x << endl;
             pq.pop();
             if(uset.find(x*2) == uset.end()){
                 pq.push(x*2);
