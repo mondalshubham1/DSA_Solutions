6d081602acf4a0958cf5abafb35f8a3132af35d5
Shubham Mondal <43444522+mondalshubham1@users.noreply.github.com>
2024-11-27 22:03:44 +0530

    Update Find Mode in Binary Search Tree.cpp
    Added comments

--- Trees/BST/Find Mode in Binary Search Tree.cpp
+++ Trees/BST/Find Mode in Binary Search Tree.cpp
@@ -18,25 +18,32 @@ public:
             return;
         }
         inorder(root->left, prev, prev_count, mode_count, ans);
-        if(root->val != prev) {                                               
-            if(prev_count > mode_count) {
-                while(ans.size() > 0) {
+
+        //-------------------------------------------- Logic to implement when I am landing at a node ----------------------------------------
+        
+        if(root->val != prev) {                      // If your current element is not equal to your previous element                                           
+            if(prev_count > mode_count) {            // Is the frequency of previous element greater than your max frequency so far?
+                while(ans.size() > 0) {              // delete the previous answer
                     ans.pop_back();
                 }
-                ans.push_back(prev);
-                mode_count = prev_count;
+                ans.push_back(prev);                 // enter the previous element as your new answer
+                mode_count = prev_count;             // your max frequency so far would be your previous element frequency
             }
-            else if(prev_count == mode_count && prev!=INT_MIN)
-                ans.push_back(prev);
-
-            prev_count = 1;
+            else if(prev_count == mode_count && prev!=INT_MIN)     // if your previous element frequency is equal to the max frequency so far 
+                ans.push_back(prev);                               //  add the prev element in the ans
+ 
+            prev_count = 1;                          
         }
-        else {
+        else {                                        // If your current element is equal to your previous element
             prev_count++;
         }
         prev = root->val;
+        
+        //------------------------------------------------------------------------------------------------------------------------------------
+        
         inorder(root->right, prev, prev_count, mode_count, ans);
     }
+
     vector<int> findMode(TreeNode* root) {
         int prev_count=0;
         int prev=INT_MIN;
